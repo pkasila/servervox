eea52577744b68936d69c9897c18746491b66941
Pavel Kasila <pavel.kasila@gmail.com>
2021-12-23 15:00:47 +0300

    Move device_information setting to servervox instead of corevox

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod renderer;
 
+use corevox::devices::device::Device;
 use corevox::network::server::VoxServer;
 use corevox::devices::science_fair_128::ScienceFair128;
 use crate::renderer::BaseRenderer;
@@ -7,10 +8,14 @@ use crate::renderer::BaseRenderer;
 fn main() {
     println!("Vox Server");
 
+    let device = Box::new(ScienceFair128 {});
+
     let serv = VoxServer {
         address: "0.0.0.0:1990".to_string(),
-        device: Box::new(ScienceFair128 {}),
-        renderer: Box::new(BaseRenderer { device_information: None }),
+        device,
+        renderer: Box::new(BaseRenderer {
+            device_information: device.device_information()
+        }),
     };
 
     println!("Starting listener on {}", serv.address);
--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -3,16 +3,12 @@ use corevox::network::messages::{DeviceInformation, VoxPack};
 use corevox::network::server::renderer::Renderer;
 
 pub struct BaseRenderer {
-    pub device_information: Option<DeviceInformation>,
+    pub device_information: DeviceInformation,
 }
 
 impl Renderer for BaseRenderer {
-    fn set_device_information(&mut self, info: DeviceInformation) {
-        self.device_information = Some(info);
-    }
-
     fn handle_vox_pack(&self, pack: VoxPack) {
-        let framerate = pack.z * self.device_information.unwrap().pov_frequency;
+        let framerate = pack.z * self.device_information.pov_frequency;
         println!("Received {} bytes (framerate needed: {})", pack.raw.capacity(), framerate);
     }
 }
