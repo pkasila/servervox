0ce6ef6cf26be25fe9deac4d9c443e633874ca52
Pavel Kasila <pavel.kasila@gmail.com>
2021-12-26 19:09:35 +0300

    Updates in rendering chain

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -27,9 +27,7 @@ impl Renderer for BaseRenderer {
 
         let mut t = Command::new("/bin/sh")
             .arg("-c")
-            .arg(format!("ffmpeg -loop {} -r {} -f mp4 -c:v h264_v4l2m2m -i pipe: -pix_fmt bgra -f fbdev /dev/fb0",
-                         self.device_information.pov_frequency * 10,
-                         self.device_information.pov_frequency * self.device_information.vox_size[2]))
+            .arg("ffmpeg -f mp4 -c:v h264_v4l2m2m -i pipe: -pix_fmt bgra -f rawvideo pipe:")
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
             .spawn()
@@ -37,6 +35,25 @@ impl Renderer for BaseRenderer {
 
         if let Some(mut stdin) = t.stdin.take() {
             stdin.write_all(&p.raw).unwrap();
+        }
+
+        let mut data: Vec<u8> = vec![];
+        t.stdout.unwrap().read_to_end(&mut data).unwrap();
+
+        let mut p = Command::new("/bin/sh")
+            .arg("-c")
+            .arg(format!("ffmpeg -f rawvideo -pix_fmt bgra -s {}x{} -r {} -i pipe: -f fbdev /dev/fb0",
+                         self.device_information.vox_size[0], self.device_information.vox_size[1],
+                         self.device_information.vox_size[2] * self.device_information.pov_frequency))
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn()
+            .unwrap();
+
+        if let Some(mut stdin) = p.stdin.take() {
+            for _ in 0..10 {
+                stdin.write_all(&data).unwrap();
+            }
         }
     }
 }
